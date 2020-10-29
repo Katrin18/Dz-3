b8b14944d6f474f296605112df8491b3a32920df
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2020-10-29 21:10:17 +0200

    js-1

---
+++ dz-5/1.js
@@ -0,0 +1,286 @@
+//1
+
+
+// let hello='hello';
+// let owu='owu';
+// let ua='ua';
+// let a=1;
+// let b=10;
+// let c=-999;
+// let d=123;
+// let e=3.14;
+// let f=2.7;
+// let k=16;
+// let HasPupil=true;
+// let HasAdmin=false;
+//
+//
+// console.log(hello);
+// alert(hello);
+// document.write(hello,'<br>');
+//
+// console.log(owu);
+// alert(owu);
+// document.write(owu,'<br>');
+//
+// console.log(ua);
+// alert(ua);
+// document.write(ua,'<br>');
+//
+//
+// console.log(a);
+// alert(a);
+// document.write(a,'<br>');
+//
+//
+// console.log(b);
+// alert(b);
+// document.write(b,'<br>');
+//
+// console.log(c);
+// alert(c);
+// document.write(c,'<br>');
+//
+//
+// console.log(d);
+// alert(d);
+// document.write(d,'<br>');
+//
+//
+// console.log(e);
+// alert(e);
+// document.write(e,'<br>');
+//
+// console.log(f);
+// alert(f);
+// document.write(f),'<br>';
+//
+// console.log(k);
+// alert(k);
+// document.write(k,'<br>');
+//
+// console.log(HasPupil);
+// alert(HasPupil);
+// document.write(HasPupil,'<br>');
+//
+//
+// console.log(HasAdmin);
+// alert(HasAdmin);
+// document.write(HasAdmin);
+// document.write('<br>','<hr>');
+
+
+//2
+
+// let hello='HELLO';
+// let owu='OWU';
+// let ua='UA';
+// let a=15;
+// let b=32;
+// let c=-101;
+// let d=321;
+// let e=10.1;
+// let f=8.8;
+// let k=4;
+// let HasPupil=false;
+// let HasAdmin=true;
+//
+//
+// console.log(hello);
+// alert(hello);
+// document.write(hello,'<br>');
+//
+// console.log(owu);
+// alert(owu);
+// document.write(owu,'<br>');
+//
+// console.log(ua);
+// alert(ua);
+// document.write(ua,'<br>');
+//
+//
+// console.log(a);
+// alert(a);
+// document.write(a,'<br>');
+//
+//
+// console.log(b);
+// alert(b);
+// document.write(b,'<br>');
+//
+// console.log(c);
+// alert(c);
+// document.write(c,'<br>');
+//
+//
+// console.log(d);
+// alert(d);
+// document.write(d,'<br>');
+//
+//
+// console.log(e);
+// alert(e);
+// document.write(e,'<br>');
+//
+// console.log(f);
+// alert(f);
+// document.write(f),'<br>';
+//
+// console.log(k);
+// alert(k);
+// document.write(k,'<br>');
+//
+// console.log(HasPupil);
+// alert(HasPupil);
+// document.write(HasPupil,'<br>');
+//
+//
+// console.log(HasAdmin);
+// alert(HasAdmin);
+// document.write(HasAdmin);
+// document.write('<br>','<hr>');
+
+
+
+
+//3
+// const user='Katya';
+// const city='Lviv';
+// const country='Ukraine';
+// const age=19;
+// const year=2020;
+// const birthday=2001;
+//
+//
+//
+// console.log(user,city,country,age,year,birthday);
+// alert(user,city,country,age,year,birthday);
+// document.write(user,'<br>',city,'<br>',country,'<br>',age,'<br>',year,'<br>',birthday);
+
+
+//4, 5, 6
+
+
+// let name1=prompt('What is first name?');
+// document.write('<br>',name1);
+// alert(name1);
+// console.log(name1);
+// let name2=prompt('What is second name?');
+// document.write('<br>',name2);
+// alert(name2);
+// console.log(name2);
+// let name3=prompt('What is third name?');
+// document.write('<br>',name3);
+// alert(name3);
+// console.log(name3,"<hr>");
+//
+//
+// let person=name1+name2+name3;
+// document.write(person,'<hr>');
+
+
+
+//7
+
+
+// let num1=+prompt("Enter number1:");
+// let num2=+prompt("Enter number2:");
+// let num3=+prompt("Enter number3:");
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+
+//8
+
+
+
+// let num1=parseInt(prompt("Enter number1:"));
+// let num2=parseInt(prompt("Enter number2:"))
+// let num3=parseInt(prompt("Enter number3:"));
+// let num4=parseInt(prompt("Enter number4:"));
+//
+//
+// let result=num1+num2+num3+num4;
+// console.log(result);
+
+
+
+//9
+
+
+// let num1=parseFloat(prompt("Enter number1:"));
+// let num2=parseFloat(prompt("Enter number2:"))
+// let num3=parseFloat(prompt("Enter number3:"));
+//
+//
+// let result=num1+num2+num3;
+// console.log(result);
+
+
+//10
+
+
+// let num1=Math.round(prompt("Enter number1:"));
+// let num2=Math.round(prompt("Enter number2:"))
+// let num3=Math.round(prompt("Enter number3:"));
+//
+//
+// let result=num1+num2+num3;
+// console.log(result);
+
+
+
+//11
+
+
+// let num1=parseInt(prompt("Enter number:"));
+// let num2=parseInt(prompt("Enter power:"));
+//
+// let res=Math.pow(num1,num2);
+// document.write(res);
+
+
+
+//12
+
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+
+//13
+
+
+// let a1=5 < 6 ;
+// let a2=5 > 6;
+// let a3=(5 == 6) ;
+// let a4=5 === 6 ;
+// let a5=10 == 10 ;
+// let a6=10 ===10 ;
+// let a7=10 < 10;
+// let a8=10 > 10 ;
+// let a9=10 !== 10 ;
+// let a10=123 ==='123' ;
+// let a11=  123 =='123';
+// console.log(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11);
+
+
+
+
+
+
+
+
+
+
+
---
+++ praktuchna/1.js
@@ -0,0 +1,129 @@
+//1
+
+
+// let str="Hello";
+// let num=123;
+// let flag=true;
+// let txt="true";
+//  console.log(typeof str);
+//  console.log(typeof num);
+//  console.log(typeof flag);
+//  console.log(typeof txt);
+
+//2
+
+// let a1=5+3;
+// let a2=5-3;
+// let a3=5*3;
+// let a4=5/3;
+// let a5=5%3;
+//
+// document.write(a1,'<br>');
+// document.write(a2,'<br>');
+// document.write(a3,'<br>');
+// document.write(a4,'<br>');
+// document.write(a5,'<br>');
+
+//3
+
+// let a6=5 % 3;
+// let a7=3 % 5;
+// let a8=5 + '3';
+// let a9='5' - 3;
+// let a10=75 + 'кг';
+//
+//
+//  document.write(a6,'<br>');
+//  document.write(a7,'<br>');
+//  document.write(a8,'<br>');
+//  document.write(a9,'<br>');
+//  document.write(a10,'<br>');
+//
+
+//4
+
+// let height=23;
+// let width=10;
+// let s=height*width;
+// document.write(s);
+
+//5
+
+// let heightC=10;
+// let dC=4;
+// let v=3.14*(dC/2)*heightC;
+// document.write(v);
+
+//6
+
+// let n=3;
+// let m=4;
+// let k=Math.pow(n,2)+Math.pow(m,2);
+// document.write(Math.sqrt(k));
+
+//7
+
+// let str="Hello world!";
+// console.log(str,"<br>");
+// alert(str,"<br>");
+// document.write(str,"<br>");
+
+//8
+
+// let FIO="Trofymyuk Kateryna Oleksandrivna";
+// let age=19;
+// let hobby="Programming";
+// alert(FIO);
+// alert(age);
+// alert(hobby);
+
+//9
+
+//  let str1="Кто ";
+// let str2="ти ";
+// let str3="такой ?";
+// let concatenation=str1+str2+str3;
+// document.write(concatenation);
+
+//10
+
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+//11
+
+// console.log(parseInt("3.14"));
+// console.log(parseInt("-7.875"));
+// console.log(parseInt("435"));
+// console.log(parseInt("Вася"));
+
+//12
+
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+//13
+
+// let num1=prompt("Enter number:");
+// let num2=prompt("Enter number:");
+// alert(parseInt(num1)+parseInt(num2));
+
+//14
+
+// let FirstName = prompt("Enter FirstName:");
+//  let SecondName = prompt("Enter SecondName:");
+//  let age=prompt("Enter age:");
+//  let str="Доброго вечера "+FirstName+" "+SecondName+", мои поздравления что вам "+age;
+//  alert(str);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
